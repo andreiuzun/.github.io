9948254deee10cfea30c7e284fb7f447b5309078
Andrei Uzun <uzun.andrei@gmail.com>
2025-12-16 22:11:26 +0200

    Switch to HashRouter for GitHub Pages

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,10 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createHashRouter } from 'react-router-dom';
 import Scan from './pages/Scan';
 import Confirm from './pages/Confirm';
 import Inventory from './pages/Inventory';
 import Layout from './components/Layout';
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
     {
         path: '/',
         element: <Layout />,
